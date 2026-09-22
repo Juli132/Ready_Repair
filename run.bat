9f993669ccbdae2e117ca98c9663f500c5dda06f
Juli132 <jlmugiwara@gmail.com>
2026-09-21 23:11:19 -0400

    package fix

--- run.bat
+++ run.bat
@@ -3,10 +3,8 @@ title Shop Diagnostics Web Server
 echo Starting the Shop Diagnostics server using the bundled JRE...
 echo.
 
-:: Automatically open the default web browser
 start http://localhost:4568
 
-
-"jre\bin\java.exe" -cp "lib\*;ready_repair.jar" server.ShopServer
+"jre\bin\java.exe" -jar "ready-repair.jar"
 
 pause
